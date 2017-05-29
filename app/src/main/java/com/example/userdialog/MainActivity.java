2ffc045fdefc2bb480dc47cd43f2a9e479d4ff6b
ksoyeon621 <ksoyeon621@naver.com>
2017-05-29 20:53:52 +0900

    #2 setup Dialog

--- app/src/main/java/com/example/userdialog/MainActivity.java
+++ app/src/main/java/com/example/userdialog/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.userdialog;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but = (Button)findViewById(R.id.but_info);
+        but.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        View dlgV = View.inflate(this/*getApplicationContext()*/,R.layout.dialog,null);
+        AlertDialog.Builder dialog= new AlertDialog.Builder(this/*getApplicationContext()*/);
+        dialog.setTitle("Input User Information");
+        dialog.setIcon(R.drawable.flamingo);
+        dialog.setView(dlgV);
+        dialog.setPositiveButton("OK",null);
+        dialog.setNegativeButton("CANCEL",null);
+        dialog.show();
     }
 }
